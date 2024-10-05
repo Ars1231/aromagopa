43efd4474023f4ae2da18f4322e6b5f259f729d5
Ars1231 <84440425+Ars1231@users.noreply.github.com>
2024-10-05 11:43:16 +0300

    Add files via upload

---
+++ RK.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double series(int n) {
+	double f = 1 / (log(n) / log(4));
+	return f;
+}
+
+
+int main() {
+	int n;
+	double summ = 0;
+	cin >> n;
+	for (int i = 2; i < n + 2; i++) cout << i - 1 << " слагаемое " << series(i) << "\n";
+	for (int i = 2; i < n + 2; i++) summ += series(i);
+	cout << "Сумма ряда " << summ << endl;
+	return 0;
+}
